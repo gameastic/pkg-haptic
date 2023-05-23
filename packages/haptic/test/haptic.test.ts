d5c517e355fe7ab4da5b6de705a58e90d58b13e2
Ashot Avagyan <avagyanah@gmail.com>
2023-05-23 12:31:12 +0400

    tests

--- packages/haptic/test/haptic.test.ts
+++ packages/haptic/test/haptic.test.ts
@@ -1,3 +1,9 @@
+import { Haptic } from '../src/index';
+
 test('haptic', () => {
-    //
+    expect(Haptic).toBeDefined();
+    expect(Haptic).toHaveProperty('resume');
+    expect(Haptic).toHaveProperty('suspend');
+    expect(Haptic).toHaveProperty('play');
+    expect(Haptic).toHaveProperty('stop');
 });
